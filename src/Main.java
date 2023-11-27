9f291ec894b6b6c13793361d0cadeb0db494b94d
Working Tech <k2785233247@gmail.com>
2023-11-27 18:12:50 +0800

    Changed welcome message in main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to DBMS design of Group 30!");
     }
 }
