89d1845389563ef28443da56e27b662746d19863
Rokidna G <qq617462323@outlook.com>
2023-11-28 14:35:42 +0800

    Ver20231128_1434 Before delete databases shipment,centre Working Tech

--- RelationalSchema/Entity/PRODUCT.sql
+++ RelationalSchema/Entity/PRODUCT.sql
@@ -2,6 +2,7 @@ CREATE TABLE PRODUCT
 (
     ProductID    NUMBER(4) PRIMARY KEY,
     ProductName  VARCHAR(50),
+    Price Number(4),
     Specification  VARCHAR(50),
     Description  VARCHAR(50),
     SellerID NUMBER(4),
