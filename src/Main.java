96f925831a789073ec9ccbb84c2feb1036e9235b
Rokidna G <qq617462323@outlook.com>
2023-11-28 19:41:09 +0800

    Ver20231128_1434 Before delete databases shipment,centre Working Tech

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
+import java.io.*;
+import java.io.Console;
+import java.io.IOException;
+import java.sql.*;
+import oracle.jdbc.driver.*;
+import oracle.sql.*;
+
 public class Main {
     public static void main(String[] args) throws SQLException {
         System.out.println("Welcome to DBMS design of Group 30!");
@@ -15,5 +22,14 @@ public class Main {
                         "jdbc:oracle:thin:@studora.comp.polyu.edu.hk:1521:dbms",username,pwd);
 
 
+    }
+
+
+    static void clearScreen() throws IOException, InterruptedException
+    {
+        if (System.getProperty("os.name").contains("Windows"))
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        else
+            System.out.print("\033[H\033[2J");
     }
 }
